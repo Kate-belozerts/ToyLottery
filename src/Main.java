a8636d33e4ec0f454cf04a411b5dd930114b7477
Kate Belozertseva <katebelozertseva@gmail.com>
2023-04-12 22:05:50 +0300

    Start program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import factory.StartFactory;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StartFactory.startFactory(5, 5);
+    }
+}
